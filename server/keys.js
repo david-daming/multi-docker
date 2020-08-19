7c31a80436169e349f32c778a842edcbceadfdaf
david-daming <david.liudaming@gmail.com>
2020-08-20 06:25:22 +0800

    initial commit

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgUser: process.env.PGUSER,
+  pdHost: process.env.PGHOST,
+  pdDataBase: process.env.PGDATABASE,
+  pdPassWord: process.env.PGPASSWORD,
+  pgPort: process.env.PGPORT,
+};
